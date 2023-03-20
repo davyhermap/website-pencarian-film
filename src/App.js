6b2b0dbf5330ced80286c283770314bf3772e812
Davy Herma Pratama <davyhermap@gmail.com>
2023-03-20 12:21:36 +0700

    Upload Project Website Pencarian Film

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import {getMovieList, searchMovie} from "./api"
+import {useEffect, useState} from "react"
+
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() =>{
+    getMovieList().then((result) => {
+    setPopularMovies(result)
+    })
+  }, [])
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return(
+        <div className="Movie-wrapper" key={i}>
+          <div className="Movie-title">{movie.title}</div>
+          <img className="Movie-image" src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}/>
+          <div className="Movie-date">Release: {movie.release_date}</div>
+          <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      )
+    })
+  }
+
+
+  const search = async (q) => {
+  if(q.length > 3){  
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+      //console.log({ query: query })
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Movie Project</h1>
+      <input 
+      placeholder="Cari film yang tersedia" 
+      className="Movie-search"
+      onChange={({target}) => search (target.value)}
+      />
+      <div className="Movie-container">
+        <PopularMovieList/>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
